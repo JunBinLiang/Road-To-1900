69d98ba0a8ac0eb381d1991569da7d0c077035d7
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-09-26 05:14:59 -0400

    Create 1860C.cpp

---
+++ Codeforce/1400/1860C.cpp
@@ -0,0 +1,85 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+
+using namespace std;  
+using ll = long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define ve vector
+
+template <typename T>
+ostream& operator <<(ostream& out, const vector<T>& a) {
+  out << "["; bool first = true;
+  for (auto v : a) { out << (first ? "" : ", "); out << v; first = 0;} 
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream& operator <<(ostream& out, const map<U, T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& p : a) { out << (first ? "" : ", "); out << p.first << ":" << p.second; first = 0;} out << "}";
+  return out;
+}
+
+//5:05 - 5:1
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    int mn = a[0];
+    vector<bool> hassmall(n);
+    for(int i = 0; i < n; i++) {
+        if(mn < a[i]) {
+            hassmall[i] = true;
+        }
+        mn = min(mn, a[i]);
+    }
+
+    int ans = 0;
+    int v = n + 1;
+    //must have small elements, and all of them must have no samller
+    for(int i = 0; i < n; i++) {
+        if(hassmall[i]) {
+            if(v > a[i]) {
+                ans++;
+            }
+            v = min(v, a[i]);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main(){
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
