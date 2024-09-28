61602a66de391a357502d7aa6c13c6381605b061
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-09-28 14:09:06 -0400

    Create 2007B.cpp

---
+++ Codeforce/900/2007B.cpp
@@ -0,0 +1,81 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+
+using namespace std;  
+using ll = long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define ve vector
+
+template <typename T>
+ostream& operator <<(ostream& out, const vector<T>& a) {
+  out << "["; bool first = true;
+  for (auto v : a) { out << (first ? "" : ", "); out << v; first = 0;} 
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream& operator <<(ostream& out, const map<U, T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& p : a) { out << (first ? "" : ", "); out << p.first << ":" << p.second; first = 0;} out << "}";
+  return out;
+}
+
+//2:03 - 2：08
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int mx = a[0];
+    for(int x : a) mx = max(mx, x);
+    for(int i = 0; i < m; i++) {
+        char op;
+        int l, r;
+        cin >> op >> l >> r;
+        if(op == '+') {
+            //cout << i << endl;
+            if(mx >= l && mx <= r) mx++;
+        } else {
+            if(mx >= l && mx <= r) mx --;
+        }
+        cout << mx << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
