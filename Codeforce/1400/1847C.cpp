29e3c5db7cdbcf841245cb288ad272eac042b114
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-10-01 04:39:38 -0400

    Create 1847C.cpp

---
+++ Codeforce/1400/1847C.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+
+using namespace std;  
+using ll = long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define ve vector
+
+template <typename T>
+ostream& operator <<(ostream& out, const vector<T>& a) {
+  out << "["; bool first = true;
+  for (auto v : a) { out << (first ? "" : ", "); out << v; first = 0;} 
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream& operator <<(ostream& out, const map<U, T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& p : a) { out << (first ? "" : ", "); out << p.first << ":" << p.second; first = 0;} out << "}";
+  return out;
+}
+
+//4:33 - 4:39
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int ans = 0;
+    int xo = 0;
+    set<int> prefix;
+    for(int i = 0; i < n; i++) {
+        xo ^= a[i];
+        ans = max(ans, xo);
+        for(auto p : prefix) {
+            ans = max(ans, xo ^ p);
+        }
+        prefix.insert(xo);
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    //ios::sync_with_stdio(false);
+    //cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
