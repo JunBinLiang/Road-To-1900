b1399ae6f0a525bd906686c338188086ab68076f
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-10-03 19:47:26 -0400

    Create 1709A.cpp

---
+++ Codeforce/800/1709A.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+
+using namespace std;  
+using ll = long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define ve vector
+
+template <typename T>
+ostream& operator <<(ostream& out, const vector<T>& a) {
+  out << "["; bool first = true;
+  for (auto v : a) { out << (first ? "" : ", "); out << v; first = 0;} 
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream& operator <<(ostream& out, const map<U, T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& p : a) { out << (first ? "" : ", "); out << p.first << ":" << p.second; first = 0;} out << "}";
+  return out;
+}
+
+//7:44 - 7:46
+void solve() {
+    int x;
+    cin >> x;
+    vector<int> a(3 + 1);
+    for(int i = 1; i <= 3; i++) {
+        cin >> a[i];
+    }
+
+    int cnt = 0;
+    while(x != 0) {
+        x = a[x];
+        cnt++;
+    }
+    if(cnt == 3) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
