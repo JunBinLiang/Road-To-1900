cfbe59bd49d3ce12faa1de20851b8c5621ef1268
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-10-14 19:51:07 -0400

    Create 逆元乘法计算模板.cpp

---
+++ 算法/数学/exgcd/逆元乘法计算模板.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+#include <random>
+#include <chrono>
+
+using namespace std;  
+using ll = long long;
+using ull = unsigned long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+
+long long gcd(long long a, long long b) {
+    if(b == 0) return a;
+    if(b > a) swap(a, b);
+    return gcd(b, a % b); // 递归求解
+}
+
+long long exgcd(long long a, long long b, long long &x, long long &y) {
+    if(b == 0) {
+        x = 1; // 设置b=0时的特殊解 
+        y = 0;
+        return a;
+    }
+    int ans = exgcd(b, a % b, x, y);
+    int t = x; // 将x2, y2换算成x1, y1
+    x = y;
+    y = t - a / b * y;
+    //cout << "exgcd " << a << " " << b << "  " << x << " "  << y << "     |" << a * x + b * y << endl;
+    return ans;
+}
+
+
+
+void solve() {
+    long long a, b;
+    cin >> a >> b;
+    long long x, y;
+    exgcd(b, 9973, x, y);
+    x %= 9973;
+    if(x < 0) x += 9973; 
+    cout << (a * x) % 9973 << endl;
+    
+}
+
+int main(){
+    int t = 1;
+    cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
