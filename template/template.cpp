e7a2b9ebd48e906311fd869d97b0ffa57aaed40a
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-10-16 17:29:12 -0400

    Create template.cpp

---
+++ template/template.cpp
@@ -0,0 +1,28 @@
+//Fenwick Tree
+const int FENWICK_SIZE = 200000 + 100;
+struct Fenwick {
+  int tree[FENWICK_SIZE];
+  void init(int n) {
+    for(int i = 0; i <= n + 1; i++) tree[i] = 0;
+  }
+
+  void update(int i, int val) {
+    i++;
+    while(i < FENWICK_SIZE){
+      tree[i] += val;
+      i += (i & -i);
+    }
+  }
+
+  int pre(int i) {
+    int sum = 0;
+    while(i>0){
+      sum += tree[i];
+      i -= ( i & -i);
+    }
+    return sum;
+  }
+  int query(int i, int j) {
+      return pre(j + 1) - pre(i);
+  }
+} tr;
