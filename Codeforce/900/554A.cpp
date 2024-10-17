7835ad7b267cae70f7ee21afca80f01d70c75490
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-10-17 14:12:04 -0400

    Create 554A.cpp

---
+++ Codeforce/900/554A.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+#include <random>
+#include <chrono>
+
+using namespace std;  
+using ll = long long;
+using ull = unsigned long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+
+
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    cout << (n + 1) * 26 - n << endl;
+}
+
+int main(){
+    int t = 1;
+    //cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
