71e0f9a3e65dd60e0a60626f6a83edd73817b432
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-10-20 21:30:14 -0400

    Update template.cpp

--- template/template.cpp
+++ template/template.cpp
@@ -141,3 +141,62 @@ namespace Graph {
         g[v].push_back({u, w, ei++});
     }
 }
+
+namespace MaxFlow {
+    const int N = 1e5 + 10, M = 2e5 + 10, INF = 1e8;
+    int n, m, S, T;
+    int f[M]; //流量
+    int d[N], cur[N]; //分层图
+    struct E {
+        int v, ei;
+    };
+    vector<E> g[N];
+    int ei = 0;
+    void add(vector<E> g[N], int u, int v, int w) {
+        g[u].push_back({v, ei});
+        f[ei++] = w;
+        g[v].push_back({u, ei});
+        f[ei++] = 0;
+    }
+
+    bool bfs() {
+        memset(d, -1, sizeof d);
+        queue<int> q;
+        q.push(S);
+        d[S] = 0;
+        cur[S] = 0;
+        while(q.size()) {
+            int u = q.front(); q.pop();
+            for(auto& e : g[u]) {
+                if(d[e.v] == -1 && f[e.ei]) {
+                    d[e.v] = d[u] + 1;
+                    cur[e.v] = 0; 
+                    q.push(e.v);
+                    if(e.v == T) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    int find(int u, int limit) {
+        if (u == T) return limit;
+        int flow = 0;
+        for (int i = cur[u]; i < g[u].size() && flow < limit; i++) {
+            int v = g[u][i].v;
+            cur[u] = i;  // 当前弧优化
+            if (d[v] == d[u] + 1 && f[g[u][i].ei]) {
+                int t = find(v, min(f[g[u][i].ei], limit - flow));
+                if (!t) d[v] = -1;
+                f[g[u][i].ei] -= t, f[g[u][i].ei ^ 1] += t, flow += t;
+            }
+        }
+        return flow;
+    }
+
+    int dinic() {
+        int r = 0, flow;
+        while (bfs()) while (flow = find(S, INF)) r += flow;
+        return r;
+    }
+}
