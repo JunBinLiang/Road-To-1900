b54b81244f8a43095a47a75a9ac5a726d016db73
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-10-29 23:44:01 -0400

    Create acw2174-费用流(模板+最小费用流).cpp

---
+++ 算法/网络流/费用流/acw2174-费用流(模板+最小费用流).cpp
@@ -0,0 +1,120 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+#include <random>
+#include <chrono>
+
+using namespace std;  
+using ll = long long;
+using ull = unsigned long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+
+const int N = 5010, M = 2e5 + 10, INF = 1e8;
+int n, m, S, T;
+int f[M], to[M], weight[M]; //流量
+int d[N]; //分层图
+int incf[N], pre[N];
+bool st[N];
+
+struct E {
+    int v, ei;
+};
+vector<E> g[N];
+int ei = 0;
+void add(vector<E> g[N], int u, int v, int cap, int w) {
+    g[u].push_back({v, ei});
+    to[ei] = v;
+    weight[ei] = w;
+    f[ei++] = cap;
+    
+    g[v].push_back({u, ei});
+    to[ei] = u;
+    weight[ei] = -w;
+    f[ei++] = 0;
+}
+
+/*
+费用流：最大流中费用最大/最小值
+
+费用: 所有边 边流量 * 边费用 和
+
+SPFA 不能处理负权回路(原图没有负环就行，不用担心残留网络)
+
+*/
+
+bool spfa() {//判断是否存在最短增广路
+    for(int i = 0; i <= n; i++) {
+        d[i] = INF;
+        st[i] = false;
+        incf[i] = 0;
+        pre[i] = -1;
+    }
+    queue<int> q;
+    q.push(S);
+    d[S] = 0;
+    incf[S] = INF;
+    while(q.size()) {
+        int u = q.front(); q.pop();
+        st[u] = false;
+        for(int i = 0; i < g[u].size(); i++) {
+            E& e = g[u][i];
+            if(f[e.ei] && d[e.v] > d[u] + weight[e.ei]) {
+                d[e.v] = d[u] + weight[e.ei];
+                pre[e.v] = e.ei;
+                incf[e.v] = min(incf[u], f[e.ei]);
+                if(!st[e.v]) {
+                    q.push(e.v);
+                    st[e.v] = true;
+                }
+            }
+        }
+    }
+    return incf[T] > 0;
+}
+
+void EK(int& flow, int& cost) {
+    while(spfa()) //如果存在最短增广路
+    {   
+        int t = incf[T]; //记录当前最短增广路的最大流量
+        flow += t, cost += t * d[T]; //累加最大流和最小费用
+        //增广路流变化
+        int u = T;
+        while(u != S) {
+            int eid = pre[u];
+            f[eid] -= t;
+            f[eid ^ 1] += t;
+            u = to[eid ^ 1];
+        }
+    }
+}
+
+void solve() {
+    cin >> n >> m >> S >> T;
+    for(int i = 0; i < m; i++) {
+        int u, v, cap, w;
+        cin >> u >> v >> cap >> w;
+        add(g, u, v, cap, w);
+    }
+    int flow = 0, cost = 0;
+    EK(flow, cost);
+    cout << flow << " " << cost << endl;
+}
+
+int main(){
+    int t = 1;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
