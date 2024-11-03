0bdf8604e219f7db8f0d412b4287b2ef6e2f0b45
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-11-03 01:53:00 -0400

    Create acw2175-后缀数组(模板).cpp

---
+++ 算法/后缀数组/acw2175-后缀数组(模板).cpp
@@ -0,0 +1,120 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include<set>
+#include<unordered_set>
+#include <string.h>
+
+
+using namespace std;
+
+/*
+后缀数组
+sa[i] : 排名第i的是第几个后缀
+rank[i] : 第i个后缀的排名
+height[i] : sa[i] 和 sa[i - 1] 的最长公共前缀
+
+height[i] = lcp(i, i - 1)
+h[i] = height[rk[i]] : 第i个后缀和排名在他前面一个后缀的最长公共前缀 
+性质 : h[i] >= h[i - 1] - 1
+*/
+
+
+struct SA {
+    string s; int n, m;
+    //vector<int> sa, rk, p, height;
+    //vector<int> discrete, second; //first[i] => 第i个后缀按前k个字母排好序后的前k个离散化数值
+    int sa[1000010], rk[1000010], p[1000010], height[1000010], discrete[1000010], second[1000010], oldDiscrete[1000010];
+    void init(string s, int N = 1000010) {
+        this -> n = s.size();
+        this -> m = 256;
+        this -> s = " " + s;
+
+        /*sa = vector<int>(N, 0);
+        rk = vector<int>(N, 0);
+        p = vector<int>(N, 0);
+        discrete = vector<int>(N, 0);
+        second = vector<int>(N, 0);
+        height = vector<int>(N, 0);*/
+    }
+
+    void getSA() {
+        for(int i = 1; i <= n; i++) {
+            discrete[i] = s[i];
+            p[discrete[i]] ++ ;
+        }
+        for (int i = 2; i <= m; i ++ ) p[i] += p[i - 1];
+        for (int i = n; i >= 1; i -- ) {
+            sa[p[discrete[i]]] = i;
+            p[discrete[i]]--;
+        }
+        
+        for(int k = 1; k <= n; k *= 2) {
+            //没有第二关键字
+            int num = 0; 
+            for (int i = n - k + 1; i <= n; i++ ) second[++num] = i;
+            for (int i = 1; i <= n; i ++ ) { 
+                if (sa[i] > k) { //sa[i] 是 sa[i] - k的第二关键字
+                    second[++num] = sa[i] - k;
+                }
+            } //已经按第二关键字排好序, second[i] => 排名第i的是第几个后缀, 这里是根据第二关键字
+                
+            //按第一关键字基数排序, 相同的会保证第二关键字的in place order 不变
+            for (int i = 1; i <= m; i++ ) p[i] = 0;
+            for (int i = 1; i <= n; i++ ) p[discrete[i]]++ ;
+            for (int i = 2; i <= m; i++ ) p[i] += p[i - 1];
+            for (int i = n; i >= 1; i-- ) {
+                sa[p[discrete[second[i]]]--] = second[i];
+                second[i] = 0;
+            }
+
+            swap(discrete, second);  //这里的second 现在保存的是老的离散化指
+            
+            //离散化
+            discrete[sa[1]] = 1, num = 1;
+            for (int i = 2; i <= n; i ++ ) {
+                //discrete[sa[i]] = (oldDiscrete[sa[i]] == oldDiscrete[sa[i - 1]] && oldDiscrete[sa[i] + k] == oldDiscrete[sa[i - 1] + k]) ? num : ++num;
+                discrete[sa[i]] = (second[sa[i]] == second[sa[i - 1]] && second[sa[i] + k] == second[sa[i - 1] + k]) ? num : ++num;
+            }
+            if (num == n) break; //完全分离，可提前break
+            m = num;  
+        }
+    }
+
+    void getHeight() {
+        for (int i = 1; i <= n; i ++ ) rk[sa[i]] = i;
+        for (int i = 1, k = 0; i <= n; i ++ ) {
+            if (rk[i] == 1) continue;
+            if (k) k -- ;
+            int j = sa[rk[i] - 1];
+            while (i + k <= n && j + k <= n && s[i + k] == s[j + k]) k ++ ;
+            height[rk[i]] = k;
+        }
+    }
+} sa;
+
+void solve() {
+    string s;
+    cin >> s;
+    sa.init(s);
+    sa.getSA();
+    sa.getHeight();
+    for(int i = 1; i <= s.size(); i++) cout << sa.sa[i] << " ";
+    cout << endl;
+    for(int i = 1; i <= s.size(); i++) cout << sa.height[i] << " ";
+    cout << endl;
+}
+
+int main(){
+    int t = 1;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
