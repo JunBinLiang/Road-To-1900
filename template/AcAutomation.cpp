28d2d41c07607a49c9c33e3550d76b0b1639896a
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-12-06 23:14:30 -0500

    Create AcAutomation.cpp

---
+++ template/AcAutomation.cpp
@@ -0,0 +1,51 @@
+struct AcAutomation {
+    int tr[10010 * 55][26];
+    int ne[10010 * 55], cnt[10010 * 55];
+    int id = 1;
+    void init() {
+        memset(tr, 0, sizeof tr);
+        memset(ne, 0, sizeof ne);
+        memset(cnt, 0, sizeof cnt);
+        id = 1;
+    }
+
+    void clear() {
+        for(int i = 0; i <= id; i++) {
+            ne[i] = 0;
+            cnt[i] = 0;
+            for(int j = 0; j < 26; j++) tr[i][j] = 0;
+        }
+        id = 1;
+    }
+
+    void add(string& s) {
+        int u = 0;
+        for(int i = 0; i < s.size(); i++) {
+            int c = s[i] - 'a';
+            if(tr[u][c] == 0) tr[u][c] = id++;
+            u = tr[u][c];
+        }
+        cnt[u]++;
+    }
+  
+    void build() {
+        queue<int> q;
+        for(int i = 0; i < 26; i++) {
+            if(tr[0][i] != 0) {
+                q.push(tr[0][i]);
+            }
+        }
+        while(q.size() > 0) {
+            int u = q.front(); q.pop();
+            for(int i = 0; i < 26; i++) {
+                int c = tr[u][i];
+                if(!c) continue;
+                int j = ne[u];
+                while(j && !tr[j][i]) j = ne[j];
+                if(tr[j][i]) j = tr[j][i];
+                ne[c] = j;
+                q.push(c);
+            }
+        }
+    }
+} ac;
