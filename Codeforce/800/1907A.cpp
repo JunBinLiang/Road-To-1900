10f8140b4df7ed51abdeba9d9e406b59c1ce94a8
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-12-16 19:18:25 -0500

    Create 1907A.cpp

---
+++ Codeforce/800/1907A.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+bool isL(char c) {
+    int d = c - 'a';
+    return d >= 0 && d < 26;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    
+    for(int i = 1; i <= 8; i++) {
+        if(s[1] - '0' == i) continue;
+        cout << s[0] << i << endl;
+    }
+
+    for(char c = 'a'; c <= 'h'; c++) {
+        if(c == s[0]) continue;
+        cout << c << s[1] << endl;
+    }
+
+}   
+
+
+int main(){
+    int t = 1;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
