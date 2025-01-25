e8809cbfbd59d2c8526824465d3915ced098bc8e
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-01-25 02:02:22 -0500

    Create 2003A.cpp

---
+++ Codeforce/800/2003A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>    
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    if(s[0] == s.back()) cout << "NO" << endl;
+    else cout << "YES" << endl;
+}   
+
+
+int main(){
+    int t = 1;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
