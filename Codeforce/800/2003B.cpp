26362ab08b5dbdfe8d9bc2da0eef74c43c961f1d
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-01-25 02:13:05 -0500

    Create 2003B.cpp

---
+++ Codeforce/800/2003B.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>    
+
+using namespace std;
+using ll = long long;
+//2:08 - 2:11
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    cout << a[n / 2] << endl;
+}   
+
+
+int main(){
+    int t = 1;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
