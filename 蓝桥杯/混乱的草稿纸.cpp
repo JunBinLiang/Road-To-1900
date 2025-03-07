8ce76567da9dc470a767026261d3c9f17fe0f17e
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-03-07 13:37:38 -0500

    Create 混乱的草稿纸.cpp

---
+++ 蓝桥杯/混乱的草稿纸.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>    
+#include <deque>  
+
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for(int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  int x = n;
+  for(int i = n - 1; i >= 0; i--) {
+    if(a[i] == x) x--;
+  }
+  cout << x << endl;
+}
+
+int main() {
+  int t = 1;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+  //cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
