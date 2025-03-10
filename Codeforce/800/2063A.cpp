8693cf5fc4ff499ad53ffd54499144abc8080ca6
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-03-10 13:28:54 -0400

    Create 2063A.cpp

---
+++ Codeforce/800/2063A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>    
+#include <deque>  
+
+using namespace std;
+
+#define ve vector
+#define pb push_back
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+
+using ll = long long;
+
+void solve() {
+    int l, r;
+    cin >> l >> r;
+    
+    int ans = r - l;
+    if(l == 1 && r == 1) ans = 1;
+    cout << ans << endl;
+}
+
+int main() {
+  int t = 1;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
