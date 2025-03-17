d4f8fb5bc523836a10fc62a36d2668c3d299a6a3
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-03-16 23:57:40 -0400

    Create 2013B.cpp

---
+++ Codeforce/900/2013B.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>    
+#include <deque>  
+
+using namespace std;
+
+#define ve vector
+#define pb push_back
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+using ll = long long;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    long long ans = a.back();
+    ans -= a[n - 2];
+    for(int i = 0; i < n - 2; i++) ans += a[i];
+    cout << ans << endl;
+}
+
+int main() {
+  int t = 1;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
