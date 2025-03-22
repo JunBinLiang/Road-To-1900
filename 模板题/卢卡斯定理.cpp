bf8f257079b8c82b4e2ef00bb270c21a0698e0bc
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-03-22 18:21:19 -0400

    Create 卢卡斯定理.cpp

---
+++ 模板题/卢卡斯定理.cpp
@@ -0,0 +1,54 @@
+//https://www.lanqiao.cn/problems/1159/learning/?page=2&first_category_id=1&second_category_id=8
+#include <iostream>
+#include <vector>
+
+class BinomModPrime {
+  int p;
+  std::vector<int> fa, ifa;
+
+  // Calculate binom(n, k) mod p for n, k < p.
+  int calc(int n, int k) {
+    if (n < k) return 0;
+    long long res = fa[n];
+    res = (res * ifa[k]) % p;
+    res = (res * ifa[n - k]) % p;
+    return res;
+  }
+
+ public:
+  BinomModPrime(int p) : p(p), fa(p), ifa(p) {
+    // Factorials mod p till p.
+    fa[0] = 1;
+    for (int i = 1; i < p; ++i) {
+      fa[i] = (long long)fa[i - 1] * i % p;
+    }
+    // Inverse of factorials mod p till p.
+    ifa[p - 1] = p - 1;  // Wilson's theorem.
+    for (int i = p - 1; i; --i) {
+      ifa[i - 1] = (long long)ifa[i] * i % p;
+    }
+  }
+
+  // Calculate binom(n, k) mod p.
+  int binomial(long long n, long long k) {
+    long long res = 1;
+    while (n || k) {
+      res = (res * calc(n % p, k % p)) % p;
+      n /= p;
+      k /= p;
+    }
+    return res;
+  }
+};
+
+int main() {
+  int t;
+  std::cin >> t;
+  while(t--) {
+    int n, k, p;
+    std::cin >> n >> k >> p;
+    BinomModPrime bm(p);
+    std::cout << bm.binomial(n, k) << std :: endl;
+  }
+  return 0;
+}
