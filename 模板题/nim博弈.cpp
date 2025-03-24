f5f98b92fd39770a29ee43f5c270243c83a0f659
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-03-24 13:52:51 -0400

    Update nim博弈.cpp

--- 模板题/nim博弈.cpp
+++ 模板题/nim博弈.cpp
@@ -1,6 +1,8 @@
 //https://cloud.tencent.com/developer/article/2033965
 #include <iostream>
 using namespace std;
+//有 n 堆石子，第 i 堆石子有 
+ai个，每次可以从某一堆中取走若干个，先后手轮流取，最后无石子可取的人负。
 //将n堆石子每堆石子的数量求异或和（ans = a[1] ^ a[2] ^ … ^a[n]），若ans == 0，先手必败，否则先手必胜。
 int main()
 {
