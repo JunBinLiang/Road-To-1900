1b84e7b533ba286bf2111ed12da1461b856cd5c8
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-03-25 02:47:39 -0400

    Update Miller_Rabin.cpp

--- 模板题/Miller_Rabin.cpp
+++ 模板题/Miller_Rabin.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 //https://blog.csdn.net/ECNU_LZJ/article/details/72675595
+//https://blog.csdn.net/qq_26131031/article/details/124150403?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0-124150403-blog-4261948.235^v43^pc_blog_bottom_relevance_base9&spm=1001.2101.3001.4242.1&utm_relevant_index=3
 //判断大素数
 
 typedef long long int ll;
