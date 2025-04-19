3c5b632deafeafb3315297255556834a49cdea04
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-04-19 15:15:04 -0400

    Create 发射火箭.cpp

---
+++ 蓝桥杯/蓝桥月赛28/发射火箭.cpp
@@ -0,0 +1,9 @@
+//https://www.lanqiao.cn/problems/20310/learning/?contest_id=254
+#include <iostream>
+using namespace std;
+int main()
+{
+  // 请在此输入您的代码
+  cout << 123 + 789 << endl;
+  return 0;
+}
