aca512bb32907017942f80dedd581b23d1a8e23d
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-07-01 20:51:39 -0400

    Update 3333-Find the Original Typed String II(H).cpp

--- Leetcode/DP/3333-Find the Original Typed String II(H).cpp
+++ Leetcode/DP/3333-Find the Original Typed String II(H).cpp
@@ -59,3 +59,62 @@ public:
         return ans;
     }
 };
+
+
+
+const int MOD = 1e9 + 7;
+class Solution {
+public:
+    int possibleStringCount(string s, int k) {
+        int n = s.size();
+        vector<int> a;
+        for(int i = 0; i < n; i++) {
+            int j = i;
+            while(j < n && s[j] == s[i]) {
+                j++;
+            }
+            a.push_back(j - i);
+            i = j - 1;
+        }
+
+        long long tot = 1;
+        for(int i = 0; i < a.size(); i++) {
+            tot *= a[i];
+            tot %= MOD;
+        }
+        if(k <= a.size()) {
+            return tot;
+        }
+
+        vector<vector<long long>> dp(a.size(), vector<long long>(k, 0));
+        for(int i = 0; i < a.size(); i++) {
+            int cnt = a[i] - 1;
+            if(i == 0) {
+                for(int j = a.size(); j <= min(k - 1, (int)(cnt + a.size())); j++) {
+                    dp[i][j] = 1;
+                }
+
+                for(int j = 1; j <= k - 1; j++) {
+                    dp[i][j] += dp[i][j - 1];
+                    dp[i][j] %= MOD;
+                }
+                continue;
+            }
+
+            for(int j = a.size(); j <= k - 1; j++) {
+                if(j <= cnt) {
+                    dp[i][j] += dp[i - 1][j];
+                } else {
+                    dp[i][j] += ((dp[i - 1][j] - dp[i - 1][j - cnt - 1] + MOD) % MOD);
+                }
+            }
+
+            for(int j = 1; j <= k - 1; j++) {
+                dp[i][j] += dp[i][j - 1];
+                dp[i][j] %= MOD;
+            }
+        }
+        
+        return (tot - dp.back()[k - 1] + MOD) % MOD;   
+    }
+};
