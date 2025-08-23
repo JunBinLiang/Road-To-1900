6cf99c99df4c37b3d6c8160fdf40c1accf93a245
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-08-23 03:43:39 -0400

    Update comb.cpp

--- template/comb.cpp
+++ template/comb.cpp
@@ -67,3 +67,13 @@ ll C(int n,int m) {
     }
   }
 }
+
+void init() {
+    C[0][0] = 1;
+    for(int i = 1;i < N; i++) {
+      C[i][0]=1;
+      for(int j = 1;j <= i;j++) {
+        C[i][j] = (C[i-1][j] + C[i-1][j-1] ) % MOD;
+      }
+    }
+}
