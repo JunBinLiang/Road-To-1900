b344e3a38963a108fecd6fbe0b92e3508fa1101a
66brother <45537132+JunBinLiang@users.noreply.github.com>
2025-09-26 01:27:32 -0400

    Update 611-Valid Triangle Number(M).cpp

--- Leetcode/Two-Pointer/611-Valid Triangle Number(M).cpp
+++ Leetcode/Two-Pointer/611-Valid Triangle Number(M).cpp
@@ -18,3 +18,25 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& a) {
+        int n = a.size();
+        sort(a.begin(), a.end());
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            int l = 0, r = i - 1;
+            while(l < r) {
+                if(a[l] + a[r] > a[i]) {
+                    ans += (r - l);
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+};
